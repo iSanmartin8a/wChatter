4744265956f6bcc198581c81c917fdccb74ff52a
Alex Sanmartín <isanmartin8a@gmail.com>
2022-11-17 13:46:41 +0100

    index

--- chatter/chatter_web/views.py
+++ chatter/chatter_web/views.py
@@ -3,8 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    texto = '''<h1>Inicio de la Librería Local</h1>
-    <p>Esta es la página de inicio de la librería local.</p>
-    Si quieres ver el index de la librería local, pulsa <a href="/catalog/">aquí</a>'''
-    # texto = 'Página principal de la librería local'
-    return HttpResponse(texto)
+    return render(request, 'index.html')
