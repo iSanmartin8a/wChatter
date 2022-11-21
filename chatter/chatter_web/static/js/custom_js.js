67ef5053fac4123f057c73631dae366bcd2832c0
Alex Sanmartín <isanmartin8a@gmail.com>
2022-11-21 13:39:20 +0100

    estilos e intento randomizar

--- chatter/chatter_web/static/js/custom_js.js
+++ chatter/chatter_web/static/js/custom_js.js
@@ -4,8 +4,8 @@ function showRandomNumber() {
   var random_number = Math.floor(Math.random() * 11) + 1;
   document.getElementById("numberCard").innerHTML = random_number;
 
-  var quotes = ["NEW THEME!", "QUESTION", "THAT'S TOUGH"];
-  var random_quote = Math.floor(Math.random() * 3);
+  var quotes = ["¡NUEVO TEMA!", "PREGUNTA", "ESTA ES DURA", "RESPONDE TU", "UFF", "NO SE YO", "TE TOCA A TI"];
+  var random_quote = Math.floor(Math.random() * 6) + 1;
   document.getElementById("cardTitle").innerHTML = quotes[random_quote];
 
 }
--- chatter/chatter_web/views.py
+++ chatter/chatter_web/views.py
@@ -1,7 +1,9 @@
+from urllib import request
 from django.http import HttpResponse
 from django.shortcuts import render
 from chatter_web.models import Theme
 from django.views.generic import ListView
+import datetime
 
 # Create your views here.
 def index(request):
@@ -13,3 +15,7 @@ def game(request):
 class Game(ListView):
     model = Theme
     paginate_by = 1
+
+    def get_queryset(self):
+        queryset = Theme.objects.all().distinct().order_by('?')
+        return queryset
