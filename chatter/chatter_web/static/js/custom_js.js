37c6c0317f74976fff3fa63b2cebb15268b83f24
Alex Sanmartín <isanmartin8a@gmail.com>
2022-11-22 14:08:33 +0100

    recolocación

--- chatter/chatter_web/static/js/custom_js.js
+++ chatter/chatter_web/static/js/custom_js.js
@@ -1,23 +1,5 @@
 
 
 function showRandomNumber() {
-  var random_number = Math.floor(Math.random() * 11) + 1;
-  document.getElementById("numberCard").innerHTML = random_number;
 
-  var quotes = ["¡NUEVO TEMA!", "PREGUNTA", "ESTA ES DURA", "RESPONDE TU", "UFF", "NO SE YO", "TE TOCA A TI"];
-  var random_quote = Math.floor(Math.random() * 6) + 1;
-  document.getElementById("cardTitle").innerHTML = quotes[random_quote];
-
-  var cat = document.querySelector('.cat');
-
-  function setProperty(duration) {
-    cat.style.setProperty('--animation-time', duration +'s');
-  }
-  
-  function changeAnimationTime() {
-    var animationDuration = Math.random();
-    setProperty(animationDuration);
-  }
-  
-  setInterval(changeAnimationTime, 1000);
 }
