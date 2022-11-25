404c6648ba6071573d0e2f4665d91504ac545c91
Alex Sanmartín <isanmartin8a@gmail.com>
2022-11-25 13:56:37 +0100

    queries van, pero no va la segunda pagina

--- chatter/chatter_web/views.py
+++ chatter/chatter_web/views.py
@@ -26,23 +26,26 @@ class Categories(ListView):
 class SearchResultsListView(ListView):
     model = Theme
     paginate_by = 1
-    template_name = 'chatter_web/theme_list.html'
+    template_name = 'chatter_web/search_themes.html'
 
     def get_queryset(self):
         query = self.request.GET.get('q')
-        query_splitted = query.split(", ")
-        print(query_splitted)
-        if query:
-            self.query = query
-            queryset = []
-            for word in query_splitted:
-                id_generos = Genre.objects.filter(name__icontains=word).values_list('id', flat=True)
-                print(id_generos)
-                object_list = Theme.objects.filter(genre__in=id_generos)
-                print(object_list)
-                queryset.append(object_list)
-            print(queryset)
-            return queryset
+        if query is not None:
+            query_splitted = query.split(", ")[:-1]
+            print(query_splitted)
+            if query:
+                self.query = query
+                queryset = Theme.objects.none()
+                for word in query_splitted:
+                    id_generos = Genre.objects.filter(name__icontains=word).values_list('id', flat=True)
+                    print(id_generos)
+                    object_list = Theme.objects.filter(genre__in=id_generos).values_list('theme', flat=True)
+                    print(object_list)
+                    queryset |= object_list
+                print(queryset)
+                return queryset
+            else:
+                return []
         else:
             return []
 
